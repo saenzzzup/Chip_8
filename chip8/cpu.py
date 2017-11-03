6e50e916f5046b3af95a280b0b16ede96b676cf1
Ricardo Saenz <Saeiounz@Ricardos-MacBook-Pro.local>
2017-11-02 19:58:47 -0600

    new way of thinking

--- chip8/cpu.py
+++ chip8/cpu.py
@@ -1,24 +1,25 @@
 class cpu(object):
 
+	gfx = [0]*2048
 	opcode = 0
 	memory = [0]*4096
-	V=[0]*16
-
-	I = 0
-	pc = 0
-
-	gfx = [0]*2048
-
-	delay_timer = 0
-	sound_timer = 0
-
-	stack = [0]*16
-	sp = 0
-
-	key = [0]*16
 
 	"""docstring for cpu"""
 	def __init__(self, arg):
-		super(Chip8, self).__init__()
-		self.arg = arg
+
+		self.timers = {
+			'delay': 0,
+			'sound': 0,
+		}
+
+		self.registers = {
+			'v': [],
+			'index': 0,
+			'sp': 0,
+			'pc': 0,
+			'rpl': []
+		}
+
+		self.stack = [0]*16
+		self.key = [0]*16
 		
