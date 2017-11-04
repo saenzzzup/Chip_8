561224409d2f45e0185c942d0b4f51d2d66b936c
RedRichard <gomric@hotmail.com>
2017-11-03 20:43:32 -0600

    Ciclo y opcodes
    Añadí los ciclos de Fetch, Decode y Execude, así como la mitad de los opcodes. Faltan 17 y revisar los que hice.

--- chip8/cpu.py
+++ chip8/cpu.py
@@ -1,3 +1,6 @@
+from stack import Stack
+from opcode import Opcode
+
 class cpu(object):
 
 	gfx = [0]*2048
@@ -5,7 +8,7 @@ class cpu(object):
 	memory = [0]*4096
 
 	"""docstring for cpu"""
-	def __init__(self, arg):
+	def __init__(self):
 
 		self.timers = {
 			'delay': 0,
@@ -13,13 +16,57 @@ class cpu(object):
 		}
 
 		self.registers = {
-			'v': [],
-			'index': 0,
+			'V': [0]*16,
 			'sp': 0,
 			'pc': 0,
-			'rpl': []
+			'I':0,
 		}
 
-		self.stack = [0]*16
-		self.key = [0]*16
-		
+		self.stack = Stack()
+
+
+	def initialize(self):
+	    return    
+	    
+	def emulateCycle(self):
+	    fetch()
+	    decodeExecute()
+	    return
+	    
+	def fetch(self):
+		#Prueba:
+		'''self.memory[self.registers['pc']] = 0xA2
+		self.memory[self.registers['pc']+1] = 0xF0'''
+
+		byte1 = self.memory[self.registers['pc']]
+		byte2 = self.memory [self.registers['pc']+1]
+		self.opcode =  byte1 << 8 | byte2
+		return
+	    
+	def decode(self):
+		if (Opcode.exists(self.opcode)):
+			print("si")
+
+	def decodeExecute(self):
+		registers, memory, stack= Opcode.execute(self.opcode, 
+			self.registers, self.memory, self.stack)
+		self.registers['pc'] += 2
+		print(self.registers['pc'])
+
+
+
+computador = cpu()
+computador.memory[0] = 0xA2
+computador.memory[1] = 0xF0
+computador.fetch()
+#print(computador.opcode)
+computador.decodeExecute()
+computador.memory[2] = 0xF0
+computador.memory[3] = 0x65
+computador.fetch()
+computador.decodeExecute()
+computador.memory[4] = 0xF0
+computador.memory[5] = 0x55
+computador.fetch()
+computador.decodeExecute()
+
---
+++ chip8/opcode.py
@@ -0,0 +1,160 @@
+class Opcode(object):
+	values = [0x0000, 0x00E0, 0x00EE, 0x1000, 0x2000, 
+			0x3000, 0x4000, 0x5000, 0x6000, 0x7000,
+			0x8000, 0x8001, 0x8002, 0x8003, 0x8004,
+			0x8005, 0x8006, 0x8007, 0x800E, 0x9000,
+			0xA000, 0xB000, 0xC000, 0xD000, 0xE00E,
+			0xE001, 0xF007, 0xF00A, 0xF015, 0xF008,
+			0xF00E, 0xF009, 0xF003, 0xF055, 0xf065]
+
+	@staticmethod
+	def exists(value):
+		if (value in Opcode.values):
+			return True
+		elif ((value & 0xF000) in Opcode.values):
+			return True
+		elif ((value & 0xF00F) in Opcode.values):
+			return True
+		elif ((value & 0xF0FF) in Opcode.values):
+			return True
+		else:
+			return False
+
+	@staticmethod
+	def execute(opcode, registers, memory, stack):
+		if((opcode & 0xF000) == 0x0000):
+			print("a")
+		elif(opcode == 0x00E0):
+			print("a")
+
+		elif(opcode == 0x00EE):
+			registers['pc'] = stack.pop()
+			print("00EE")
+
+		elif((opcode & 0xF000) == 0x1000):
+			print("1NNN")
+
+		elif((opcode & 0xF000) == 0x2000):
+			stack.push(registers['pc'])
+			pc = opcode & 0x0FFF
+			print("2NNN")
+
+
+		elif((opcode & 0xF000) == 0x3000):
+			if (registers['V'][(opcode & 0x0F00) >> 12] == memory[(opcode & 0x00FF) >> 12]):
+				registers['pc'] += 1
+
+			print("3XNN")
+
+		elif((opcode & 0xF000) == 0x4000):
+			if (registers['V'][(opcode & 0x0F00) >> 12] != memory[(opcode & 0x00FF) >> 12]):
+				registers['pc'] += 1
+			print("4XNN")
+
+		elif((opcode & 0xF000) == 0x5000):
+			if (registers['V'][(opcode & 0x0F00) >> 12] == registers['V'][(opcode & 0x00F0) >> 12]):
+				registers['pc'] += 1
+			print("5XY0")
+
+		elif((opcode & 0xF000) == 0x6000):
+			registers['V'][(opcode & 0x0F00)>>12] = ((opcode & 0x00FF)>>8)
+			print("6XNN")
+
+		elif((opcode & 0xF000) == 0x7000):
+			registers['V'][(opcode & 0x0F00)>>12] += ((opcode & 0x00FF)>>8)
+			print("7XNN")
+
+		elif((opcode & 0xF00F) == 0x8000):
+			registers['V'][(opcode & 0x0F00)>>12] = registers['V'][(opcode & 0x00F0)>>12]
+			print("8XY0")
+
+		elif((opcode & 0xF00F) == 0x8001):
+			aux = registers['V'][(opcode & 0x0F00)>>12] | registers['V'][(opcode & 0x00F0)>>12]
+			registers['V'][(opcode & 0x0F00)>>12] = aux
+			print("8XY1")
+
+		elif((opcode & 0xF00F) == 0x8002):
+			aux = registers['V'][(opcode & 0x0F00)>>12] & registers['V'][(opcode & 0x00F0)>>12]
+			registers['V'][(opcode & 0x0F00)>>12] = aux
+			print("8XY2")
+
+		elif((opcode & 0xF00F) == 0x8003):
+			a = registers['V'][(opcode & 0x0F00)>>12]
+			b = registers['V'][(opcode & 0x00F0)>>12]
+			registers['V'][(opcode & 0x0F00)>>12] = (a and not b) or (not a and b)
+			print("8XY3")
+
+		#Añadir carry
+		elif((opcode & 0xF00F) == 0x8004):
+			registers['V'][(opcode & 0x0F00)>>12] += registers['V'][(opcode & 0x00F0)>>12]
+			print("8XY4")
+
+		elif((opcode & 0xF00F) == 0x8005):
+			print("8XY5")
+
+		elif((opcode & 0xF00F) == 0x8006):
+			print("8XY6")
+
+		elif((opcode & 0xF00F) == 0x8007):
+			print("8XY7")
+
+		elif((opcode & 0xF00F) == 0x800E):
+			print("8XYE")
+
+		elif((opcode & 0xF000) == 0x9000):
+			if (registers['V'][(opcode & 0x0F00)>>12] != registers['V'][(opcode&0x00F0)>>12]):
+				registers['pc'] += 1
+			print("9XY0")
+	
+		elif((opcode & 0xF000) == 0xA000):
+			registers['I'] = opcode & 0x0FFF
+			print('ANNN')
+
+		elif((opcode & 0xF000) == 0xB000):
+			registers['pc'] = registers['v'][0] + (opcode & 0x0FFF)
+			print("BNNN")
+
+		elif((opcode & 0xF000) == 0xC000):
+			print("a")
+
+		elif((opcode & 0xF000) == 0xD000):
+			print("a")
+
+		elif((opcode & 0xF00F) == 0xE00E):
+			print("a")
+
+		elif((opcode & 0xF00F) == 0xE001):
+			print("EXA1")
+
+		elif((opcode & 0xF00F) == 0xF007):
+			print("FX07")
+
+		elif((opcode & 0xF00F) == 0xF00A):
+			print("FX0A")
+
+		elif((opcode & 0xF0FF) == 0xF015):
+			print("FX15")
+
+		elif((opcode & 0xF00F) == 0xF008):
+			print("FX18")
+
+		elif((opcode & 0xF00F) == 0xF00E):
+			print("FX1E")
+
+		elif((opcode & 0xF00F) == 0xF009):
+			print("FX29")
+
+		elif((opcode & 0xF00F) == 0xF003):
+			print("FX33")
+
+		elif((opcode & 0xF0FF) == 0xF055):
+			print("FX55")
+
+		elif((opcode & 0xF0FF) == 0xF065):
+			print("FX65")
+
+		else:
+			print("Error")
+		return registers, memory, stack
+
+
